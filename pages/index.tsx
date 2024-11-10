2cd0d402ba23456fb59fe134c0e613fb31f1342f
James Olewiler <james@olewilers.com>
2024-11-10 15:31:37 -0800

    Per User Isloation

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const client = generateClient<Schema>();
 
 export default function App() {
     
-  const { signOut } = useAuthenticator();
+  const { user, signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   function listTodos() {
@@ -32,7 +32,7 @@ export default function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
